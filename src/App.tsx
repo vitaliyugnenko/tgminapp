23e0490af41f1c7572f5fc4e2146428f39af099e
Your Name <you@example.com>
2024-07-15 16:43:57 +0300

    change ton-connect

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
+// @ts-nocheck
+
 import blumLogo from "./assets/Blum.png";
 import telegramLogo from "./assets/telegram.png";
 import twitterLogo from "./assets/twitter.png";
 import discordLogo from "./assets/discord.png";
 import "./App.css";
 import { TonConnectButton } from "@tonconnect/ui-react";
+import { useTonWallet } from "@tonconnect/ui-react";
 
 function App() {
+  const wallet = useTonWallet();
+
   return (
     <>
       <div>
@@ -29,7 +34,14 @@ function App() {
         </a>
       </div>
       <h1>GET FREE BLUM TOKENS</h1>
-      <TonConnectButton />
+      {wallet ? (
+        <div>
+          <span>Connected wallet: {wallet.name}</span>
+          <span>Device: {wallet.device.appName}</span>
+        </div>
+      ) : (
+        <TonConnectButton />
+      )}
     </>
   );
 }
